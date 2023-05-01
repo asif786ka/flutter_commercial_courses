4b0576e21b7eb24ce066aa376f69df4eff42305c
asifroadzen <asif@roadzen.io>
2023-05-01 16:58:52 +0530

    Initial commit.

---
+++ android/app/src/main/kotlin/com/example/flutter_courses_blocs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_courses_blocs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "9dc4772f29bc8ea6815d08313e064148";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
